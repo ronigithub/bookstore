03dfa3422b430d5b50f5156dbaae472a9eeee068
Roni <ronicse2011@gmail.com>
2018-09-06 18:28:31 +0600

    Modified Admin module

--- application/modules/Admin/controllers/Admin.php
+++ application/modules/Admin/controllers/Admin.php
@@ -10,10 +10,21 @@ class Admin extends MY_Controller
 	function __construct()
 	{
 		parent::__construct();
+		$this->load->module(array(
+			'Books'
+		));
 
 	}
 
 	function index(){
 		$this->template->call_admin_template();
+	}
+
+	function books(){
+		$this->books->display_books();
+	}
+
+	function addBook(){
+		$this->books->addBook();
 	}
 }
--- application/modules/Template/views/admin_template_v.php
+++ application/modules/Template/views/admin_template_v.php
@@ -275,8 +275,8 @@ desired effect
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
-        Page Header
-        <small>Optional description</small>
+        <?php echo $page_header; ?>
+        <small><?php echo $description; ?></small>
       </h1>
       <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
@@ -290,6 +290,8 @@ desired effect
       <!--------------------------
         | Your Page Content Here |
         -------------------------->
+
+        <?php $this->load->view($content_view); ?>
 
     </section>
     <!-- /.content -->
